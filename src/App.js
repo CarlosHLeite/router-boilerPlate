832d6f75d774ac6484abf27b2d9831bba1f8bc42
CarlosHLeite <carlosleiteh@gmail.com>
2022-03-29 23:51:43 -0300

    links done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import About from './pages/About';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Error from './pages/Error';
+import SharedLayout from './pages/SharedLayout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      
+      <Routes>
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/products' element={<Products />}/>
+          <Route path='*' element={<Error/>} />
+        </Route>
+      </Routes>
+      
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+
+import Navbar from '../componets/Navbar'
+
+function Home() {
+  return (
+    <section className='section'>
+        <h1>Home</h1>
+    </section>     
+  )
+}
+
+export default Home
